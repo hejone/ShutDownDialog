3bfee16791f80cfbeba428553e849bc39b25aea7
Joni Heinonen <joni.heinonen@gmail.com>
2021-12-07 17:09:55 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace ShutdownDialog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Left = SystemParameters.PrimaryScreenWidth / 2;
+            Top = SystemParameters.PrimaryScreenHeight / 3;
+            Topmost = true;
+        }
+
+        private void lostFocus(object sender, RoutedEventArgs e)
+        {
+            Topmost = true;
+            Activate();
+        }
+
+
+        private void shutDownClick(object sender, RoutedEventArgs e)
+        {   
+            ProcessStartInfo startInfo = new ProcessStartInfo() { 
+                WindowStyle = ProcessWindowStyle.Hidden,
+                FileName = "cmd.exe",
+                Arguments = @"/C C:\Windows\System32\shutdown.exe /s /t 0"
+            };
+            Process process = new Process() {
+                StartInfo = startInfo
+            };
+            process.Start();
+        }
+
+        private void cancelClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+## Shutdown dialog
+Very simple shutdown dialog to make sure user wants to shutdown the computer.
+## But why?
+This uses the Windows command to actually shut down the computer, not hibernate it.
+This should prevent some writes to SSD. Also, makes sure user don't accidentally shut down
+the computer using a shortcut to the command (*may have happened to the creator a few times* 😉)

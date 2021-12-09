90bdeb5873770e3d0d75f5b430a3223038d791dd
Joni Heinonen <joni.heinonen@gmail.com>
2021-12-10 00:50:18 +0200

    Added timer
    Now automatically shuts down the computer after 30s

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,31 +13,31 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.Windows.Threading;
 
 namespace ShutdownDialog
-{
+{   
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {   
+        private int timer = 30;
         public MainWindow()
         {
             InitializeComponent();
             Left = SystemParameters.PrimaryScreenWidth / 2;
             Top = SystemParameters.PrimaryScreenHeight / 3;
             Topmost = true;
+
+            DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0,0,1);
+            dispatcherTimer.Start();
         }
 
-        private void lostFocus(object sender, RoutedEventArgs e)
+        private void Shutdown()
         {
-            Topmost = true;
-            Activate();
-        }
-
-
-        private void shutDownClick(object sender, RoutedEventArgs e)
-        {   
             ProcessStartInfo startInfo = new ProcessStartInfo() { 
                 WindowStyle = ProcessWindowStyle.Hidden,
                 FileName = "cmd.exe",
@@ -47,6 +47,26 @@ namespace ShutdownDialog
                 StartInfo = startInfo
             };
             process.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {   
+           timer -= 1;
+            countDownLabel.Text = $"{timer} s";
+            if(timer == 0) {
+                Shutdown();
+            }
+        }
+
+        private void lostFocus(object sender, RoutedEventArgs e)
+        {
+            Topmost = true;
+            Activate();
+        }
+
+        private void shutDownClick(object sender, RoutedEventArgs e)
+        {   
+            Shutdown();
         }
 
         private void cancelClick(object sender, RoutedEventArgs e)
